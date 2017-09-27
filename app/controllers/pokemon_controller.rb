ad61f89a5e74fd522415751a8f1ebf896a8375e4
Anthematics <jasonharder2@gmail.com>
2017-09-27 16:26:02 -0400

    everything works cept data presentation

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,15 +3,17 @@ class PokemonController < ApplicationController
 		respoke = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
 		pokebody = JSON.parse(respoke.body)
 
-		resgif = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{params[:name]}&rating=g")
-		gifbody = JSON.parse(resgif.body)
-
-
-		gif_url = gifbody["data"][0]["url"]
 		id = pokebody ["id"]
 		name = pokebody["name"]
 		types = pokebody["types"]
 
-		render json: {name: name , types: [types] , id: [id]}
+
+		resgif = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{name}&rating=g")
+		gifbody = JSON.parse(resgif.body)
+
+		gif_url = gifbody["data"][0]["url"]
+
+
+		render json: {name: name , types: types , id: [id]}
 	end
 end
